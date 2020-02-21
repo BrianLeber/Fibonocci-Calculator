d9229323d45dd6ab1332588f9c59baf16b6c1b40
BrianLeber <59613693+BrianLeber@users.noreply.github.com>
2020-02-21 15:37:44 -0600

    Functional webapp

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 // Fibonocci array builder and scrum estimator
 // ###########################################
 
+// Set Variables ____________________________________________
+
+let fibonocci = [0, 1]; // Set the first two numbers in the fibonocci sequence (all others can be deduced from there)
+let i = 2; // set i to the value of the next fibonocci number to index: position 2
+// let x = 1; // Set a number to round to the fibonocci sequence
+
+var fibResult = document.querySelector("#fibResult");
+var fibCalc = document.querySelector("#fibCalculate");
+var fibInput = document.querySelector("#fibInput");
+
+// Set listeners
+fibCalc.addEventListener("click", function() {
+  x = fibInput.value;
+  // Run function
+  appendFib(i, x);
+  calculateFibIndex();
+  for (i = 1; i < fibonocci.length; i++) {
+    console.log("index " + i + ": " + fibonocci[i] + " | " + fibonocci[i] / i);
+  }
+});
+
+// declare functions _______________________________________
+
 // Add numerical punctuation to numbers
 function thousands_separators(num) {
   var num_parts = num.toString().split(".");
@@ -8,25 +31,19 @@ function thousands_separators(num) {
   return num_parts.join(".");
 }
 
-// Set the first two numbers in the fibonocci sequence (all others can be deduced from there)
-let fibonocci = [0, 1];
-
-// set i to the value of the next fibonocci number to index: position 2
-let i = 2;
-
-// Set a number to round to the fibonocci sequence
-let x = 100;
-
 // declare function: add calculations to the fibonocci (the sum of the two previous results) array until it is larger than X, starting with position 2 (the first we can formulaically calculate)
-function appendFib(i) {
+function appendFib(i, x) {
   while (fibonocci[fibonocci.length - 1] < x) {
     fibonocci[i] = fibonocci[i - 2] + fibonocci[i - 1];
     i++;
   }
+  console.log();
 }
 
 // declare function: round number to nearest fibonocci index
-function calculateFibIndex(i) {
+function calculateFibIndex() {
+  console.log(x);
+  i = 2;
   while (fibonocci[i - 1] < x) {
     fibonocci[i] = fibonocci[i - 2] + fibonocci[i - 1];
     i++;
@@ -42,7 +59,7 @@ function calculateFibIndex(i) {
       thousands_separators(x - roundDown) +
       " less than " +
       thousands_separators(x);
-    document.querySelector("body").innerHTML = y;
+    fibResult.innerHTML = y;
   } else {
     let y =
       thousands_separators(roundUp) +
@@ -50,11 +67,6 @@ function calculateFibIndex(i) {
       thousands_separators(roundUp - x) +
       " more than " +
       thousands_separators(x);
-    document.querySelector("body").innerHTML = y;
+    fibResult.innerHTML = y;
   }
 }
-
-// Run function
-appendFib(i);
-
-// for (i = 1; i < fibonocci.length; i++) {  console.log("index " + i + ": " + fibonocci[i] + " | " + fibonocci[i] / i);}
